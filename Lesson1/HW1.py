ed773be6d320161b0d07c88339f83f60f59efab6
EvheniiTkachuk <74929574+EvheniiTkachuk@users.noreply.github.com>
2020-12-04 17:08:55 +0200

    Add files via upload

---
+++ Lesson1/HW1.py
@@ -0,0 +1,17 @@
+#task2
+
+# Hello World
+print("Hello, World")
+
+# Hello, World__I'm here
+print("Hello, World", "I\'m here", sep='__')
+
+# Hello, World__I'm here__and i start learning Pyhton!!!
+print("Hello, World", "I\'m here", "and i start learning Pyhton", sep='__', end='!!!')
+
+
+
+#task3
+
+print("\n\n#########\n#\t\t#\n#\t\t#\n#\t\t#\n#########")
+print("\n\n#\t\t#\n#\t\t#\n#########\n#\t\t#\n#\t\t#\n")
