dbfda2292ab2ea8f11c31e6a227938aac90c0616
EvheniiTkachuk <74929574+EvheniiTkachuk@users.noreply.github.com>
2020-12-05 11:27:44 +0200

    Add files via upload

--- Lesson5/task3.py
+++ Lesson5/task3.py
@@ -1,12 +1,13 @@
 # Extracting numbers.
-# Make a list that contains all integers from 1 to 100, then find all integers from the list that are divisible by 7 but not a multiple of 5, and store them in a separate list. Finally, print the list.
+# Make a list that contains all integers from 1 to 100, then find all integers from the list that are divisible by 7 but
+# not a multiple of 5, and store them in a separate list. Finally, print the list.
 # Constraint: use only while loop for iteration
 
 list1 = list(range(1, 101))
 listRes = []
 i = 0
 while i < len(list1):
-    if list1[i] % 7 == 0 and list1[i] % 5 != 0:
+    if not list1[i] % 7 and list1[i] % 5:
         listRes.append(list1[i])
     i += 1
 print(f'Result list that are divisible by 7 but not a multiple of 5:  {listRes}')
